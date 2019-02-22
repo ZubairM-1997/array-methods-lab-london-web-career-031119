aae0f42b5778093432afc2d6d1d031ae89110b76
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-22 19:41:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
 def using_include(array, element)
+  array.include?("element")
 
 end
 
 def using_sort(array)
+  array.Sort
 
 end
 
 def using_reverse(array)
+  array.Reverse
 
 end
 
 def using_first(array)
+  array.First
 
 end
 
 def using_last(array)
+  array.Last
 
 end
 
 def using_size(array)
+  array.Size
 
 end
